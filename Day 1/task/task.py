f1a3ad05ec03ad9fcc658b0c81d13818a597f45f
Banteas <costantinos1987@hotmail.com>
2025-08-19 17:57:16 +0200

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Hello Stranger\nWelcome to the Band Name Generator")
+city_name = input("What is the name of the city you grew up in?: ")
+pet_name = input("What is the name of your pet?: ")
+
+print("Your band name could be: " + city_name + " " + pet_name)
