e8a0a7280f078c9ee7d5c8783554fc0cc334fdf4
Banteas <costantinos1987@hotmail.com>
2025-10-07 19:27:59 +0200

    Refactor phonetic name generation into a function
    Refactor phonetic name generation into a function and use of a while True loop with error handling (try/except) to keep asking until the user enters valid input.

--- NATO-alphabet-start/NATO-alphabet-start/main.py
+++ NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -1,9 +1,18 @@
-
 import pandas as pd
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
 phonet_dict = {row.letter:row.code for (index,row) in data.iterrows()}
+def generate_phonetic():
+    while True:
+        name_input = input("Enter a name: ").upper()
+        try:
+            output_name = [phonet_dict[letter] for letter in name_input]
+        except KeyError:
+            print("Invalid input. Please enter only alphabet letters.")
+            generate_phonetic()
+        else:
+            print(output_name)
+            break
 
-name_input = input("Enter a name: ").upper()
-output_name = [phonet_dict[letter] for letter in name_input]
-print(output_name)
+
+generate_phonetic()
